b7a5e412dff44643e958de62823234dc660a4444
garfield890 <agrawal.anay@gmail.com>
2025-01-23 10:31:35 -0800

    basic commands in js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,28 @@
-console.log("Hello, World!")
+console.log("Hello, World!");
+
+let name = "John";
+let surname = "Smith";
+
+console.log(name);
+console.log(surname)
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+
+console.log((3 + 2) - 76 * (1 + 1))
+console.log(34 + 45);
+
+console.log(1+2+3+4+5+6);
+console.log((4+6+9) / 77);
+
+let a = 10;
+console.log(a);
+let b = 9 * a;
+console.log(b);
+
+console.log((57-13)/57);

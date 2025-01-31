8dda163e927eeca7eecd51d6f3f112ab59b7e8d6
garfield890 <agrawal.anay@gmail.com>
2025-01-31 00:30:18 -0800

    completed step 3 of calc

--- odin-calculator/script.js
+++ odin-calculator/script.js
@@ -14,6 +14,20 @@ function divide(val1, val2) {
     return val1 / val2;
 }
 
+function operate(val1, operation, val2) {
+    if (operation == "+") {
+        return add(val1, val2);
+    } else if (operation == "-") {
+        return subtract(val1, val2);
+    } else if (operation == "*") {
+        return multiply(val1, val2);
+    } else if (operation == "/") {
+        return divide(val1, val2);
+    } else {
+        return "INVALID COMMAND";
+    }
+}
+
 let val1 = 0;
 let val2 = 0;
 let operation = "+";

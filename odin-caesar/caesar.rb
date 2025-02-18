6980e6b2136f0e3ff2f9a2048abfe3de8afc4cee
garfield890 <agrawal.anay@gmail.com>
2025-02-17 23:27:22 -0800

    finished odin-caesar cipher

--- odin-caesar/caesar.rb
+++ odin-caesar/caesar.rb
@@ -1,3 +1,21 @@
-def caesarCipher
-  
+def caesarCipher(str, vals)
+  char_vals = []
+  str.each_char do |character|
+    if (character == " ")
+      char_vals.push(1000)
+      next
+    end
+    char_vals.push(((character.ord - 'a'.ord + vals) % 26) + 'a'.ord)
+  end
+  newStr = ""
+  char_vals.each do |val|
+    if (val == 1000)
+      newStr += " "
+      next
+    end
+    newStr += val.chr
+  end
+  return newStr
 end
+
+puts caesarCipher("what a string", 5)

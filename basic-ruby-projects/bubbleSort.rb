09ec3aa3af94f4450395495b2518a94f2b5b4edd
garfield890 <agrawal.anay@gmail.com>
2025-02-19 10:46:51 -0800

    updated logic to not use !

--- basic-ruby-projects/bubbleSort.rb
+++ basic-ruby-projects/bubbleSort.rb
@@ -1,7 +1,7 @@
 def bubbleSort (arr)
   for i in (0..arr.length-1)
     arr.each_with_index do |val, index|
-      if (index != arr.length-1)
+      if (index < arr.length-1)
         if (arr[index] > arr[index+1]) 
           arr[index], arr[index+1] = arr[index+1], arr[index]
         end

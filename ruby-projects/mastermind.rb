aa134e17c026dc45d2bed04cc0f2a8b54f5880ec
garfield890 <agrawal.anay@gmail.com>
2025-02-26 00:30:17 -0800

    fixed mastermind w/ rubocop and configured changed

--- ruby-projects/mastermind.rb
+++ ruby-projects/mastermind.rb
@@ -1,18 +1,17 @@
 class Mastermind
-  def initialize() 
-    available_colors = ['R', 'B', 'G', 'Y', 'O', 'P', 'W']
-    @sequence_array = available_colors.shuffle.first(4)  # Pick 4 unique colors
+  def initialize
+    available_colors = %w[R B G Y O P W]
+    @sequence_array = available_colors.sample(4) # Pick 4 unique colors
     @lives = 12
   end
 
-  def checkGuess(arr) 
-    if (arr == @sequence_array)
-      puts "You win!!" + " It took you #{12-@lives+1} guesses! Good work."
-      return true
+  def check_guess(arr)
+    if arr == @sequence_array
+      puts "You win!! It took you #{12 - @lives + 1} guesses! Good work."
     else
       @lives -= 1
       print "Feedback: "
-      for i in (0..3)
+      (0..3).each do |i|
         if @sequence_array[i] == arr[i]
           print "G"
         elsif @sequence_array.include?(arr[i])
@@ -23,21 +22,18 @@ class Mastermind
       end
       puts
 
-      unless @lives == 0
+      unless @lives.zero?
         puts "You have #{@lives} lives left!"
         return false
       end
       puts "You have lost all your lives!"
-      return true
     end
+    true
   end
 end
-
 
 puts "Welcome to the Mastermind Game! The computer has chosen a code. You must guess!"
 puts
 game = Mastermind.new
-until game.checkGuess(gets.chomp.chars)
-  puts "Please make your next guess (4 characters, no spaces):"
-end
+puts "Please make your next guess (4 characters, no spaces):" until game.check_guess(gets.chomp.chars)
 puts "Game Over!"

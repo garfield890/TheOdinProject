44f0d92c432b05c01cf16f685cf406c1ff7fc570
garfield890 <agrawal.anay@gmail.com>
2025-02-26 10:43:21 -0800

    started file handling, input/output from file

---
+++ ruby-files-practice/files.rb
@@ -0,0 +1,18 @@
+$stdout.puts "Hello world"
+$stdin.gets
+
+io = IO.new(1)
+io.puts "Hello world!"
+
+num1 = IO.sysopen 'input.txt'
+read = IO.new(num1)
+num2 = IO.sysopen 'output.txt', 'w'
+write = IO.new(num2)
+
+puts read.gets
+puts read.eof?
+read.rewind
+puts read.gets
+
+write.puts "Hello world"
+write.puts 'goodbye world'

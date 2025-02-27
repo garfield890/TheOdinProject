179ca7c4ca9c1ce8cbfd79fbf66fe05976874a99
garfield890 <agrawal.anay@gmail.com>
2025-02-27 10:25:41 -0800

    json and yaml

--- ruby-files-practice/files.rb
+++ ruby-files-practice/files.rb
@@ -1,20 +1,29 @@
-$stdout.puts "Hello world"
-$stdin.gets
+# $stdout.puts "Hello world"
+# $stdin.gets
 
-io = IO.new(1)
-io.puts "Hello world!"
+# io = IO.new(1)
+# io.puts "Hello world!"
 
-num1 = IO.sysopen 'input.txt'
-read = IO.new(num1)
-num2 = IO.sysopen 'output.txt', 'w'
-write = IO.new(num2)
+# num1 = IO.sysopen 'input.txt'
+# read = IO.new(num1)
+# num2 = IO.sysopen 'output.txt', 'w'
+# write = IO.new(num2)
 
-puts read.gets
-puts read.eof?
-read.rewind
-puts read.gets
+# puts read.gets
+# puts read.eof?
+# read.rewind
+# puts read.gets
 
-write.puts "Hello world"
-write.puts 'goodbye world'
+# write.puts "Hello world"
+# write.puts 'goodbye world'
 
-File.open("input.txt")
+File.open("input.txt", "r") do |file|
+  for line in file.readlines()
+    puts line
+  end
+end
+
+File.open("output.txt", "r+") do |file|
+  file.readline()
+  file.write("\nYou are a bold one...")
+end

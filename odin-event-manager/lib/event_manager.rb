1a1270d327fec0b88b6c4cfa35b15f7b83bb4960
garfield890 <agrawal.anay@gmail.com>
2025-02-27 23:24:36 -0800

    fixed with rubocop

--- odin-event-manager/lib/event_manager.rb
+++ odin-event-manager/lib/event_manager.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
@@ -7,28 +9,20 @@ civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 puts 'Event Manager Initialized!'
 
-# contents = File.read('event_attendees.csv') if File.exist? 'event_attendees.csv'
-
-# File.readlines('event_attendees.csv').each_with_index do |line, index|
-#   next if index == 0
-#   columns = line.split(",")
-#   puts columns[2]
-# end
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 def clean_phone_numbers(phone_number)
-  cleaned_phone_number = phone_number.gsub("-", "")
+  cleaned_phone_number = phone_number.gsub('-', '')
   if cleaned_phone_number.length < 10
-    return "000-000-0000"
+    '000-000-0000'
   elsif cleaned_phone_number.length == 10
-    return cleaned_phone_number
-  elsif cleaned_phone_number.length == 11 && cleaned_phone_number[0] == "1"
-    return cleaned_phone_number[1..10]
-  else 
-    return "000-000-0000"
+    cleaned_phone_number
+  elsif cleaned_phone_number.length == 11 && cleaned_phone_number[0] == '1'
+    cleaned_phone_number[1..10]
+  else
+    '000-000-0000'
   end
 end
 
@@ -40,14 +34,14 @@ def legislators_by_zipcode(zip)
     civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -56,9 +50,6 @@ def save_thank_you_letter(id,form_letter)
     file.puts form_letter
   end
 end
-
-
-puts 'EventManager initialized.'
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -78,8 +69,8 @@ contents.each do |row|
 
   zipcode = clean_zipcode(row[:zipcode])
   phone_number = clean_phone_numbers(phone_number)
-  parsed_time = Time.strptime(hour, "%m/%d/%y %H:%M")  # Parse using correct format
-  hour = parsed_time.hour  # Extract hour (0-23)
+  parsed_time = Time.strptime(hour, '%m/%d/%y %H:%M') # Parse using correct format
+  hour = parsed_time.hour # Extract hour (0-23)
   day = parsed_time.wday
   hour_counts[hour] += 1
   day_counts[day] += 1
@@ -87,11 +78,11 @@ contents.each do |row|
   legislators = legislators_by_zipcode(zipcode)
   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id,form_letter)
+  save_thank_you_letter(id, form_letter)
 end
 
-sorted_hours = hour_counts.sort_by { |hour, count| -count }
-sorted_days = day_counts.sort_by { |day, count| -count }
+sorted_hours = hour_counts.sort_by { |_hour, count| -count }
+sorted_days = day_counts.sort_by { |_day, count| -count }
 sorted_hours.each do |hour, count|
   puts "Hour #{hour}: #{count} registrations"
 end

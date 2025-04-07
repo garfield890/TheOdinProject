a1441a65e9a47138024eabd5ee05573b8990c3e3
garfield890 <agrawal.anay@gmail.com>
2025-04-07 10:47:22 -0700

    committing everything

---
+++ ruby-misc-practice/pattern_matching.rb
@@ -0,0 +1,15 @@
+grade = 'C'
+
+case grade
+in 'A' then puts 'Amazing effort'
+in 'B' then puts 'Good work'
+in 'C' then puts 'Well done'
+in 'D' then puts 'Room for improvement'
+else puts 'See me'
+end
+
+login = { username: 'hornby', password: 'iliketrains' }
+
+login => { username: username }
+
+puts "Logged in with username #{username}"

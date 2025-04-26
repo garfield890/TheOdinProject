d1d75a682b17a498dedcab0eabcdaef8642e2229
garfield890 <agrawal.anay@gmail.com>
2025-04-26 15:04:03 -0700

    working through pattern pattern_matching

--- ruby-misc-practice/pattern_matching.rb
+++ ruby-misc-practice/pattern_matching.rb
@@ -8,8 +8,36 @@ in 'D' then puts 'Room for improvement'
 else puts 'See me'
 end
 
-login = { username: 'hornby', password: 'iliketrains' }
+# login = { username: 'hornby', password: 'iliketrains' }
 
-login => { username: username }
+# login => { username: username }
 
-puts result
+case grade
+in String then puts 'this is a String'
+in Integer then puts 'this is an integer'
+in TrueClass then puts 'this is a boolean'
+end
+
+age = 15
+
+case age
+in a
+  puts a
+end
+
+case 3
+in 3 => age
+  puts age
+end
+
+case 0
+in 0 | 1 | 2
+  puts :match
+end
+
+value = true
+
+case 0
+in 0 if value
+  puts :match
+end
